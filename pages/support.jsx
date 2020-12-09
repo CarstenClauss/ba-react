a48da5f4d88e334a9d3e5a87c64bf4d14bbd906a
CarstenClauss <carsten.clauss@farbenmeer.de>
2020-12-09 18:43:56 +0100

    feat: Add tbds

--- pages/support.jsx
+++ pages/support.jsx
@@ -18,8 +18,52 @@ const Description = styled.div`
   margin: 0 0 0 2em;
 `;
 
+const ComponentContainer = styled.div`
+  display:flex;
+  flex-direction: column;
+  justify-content: space-around;
+  width: calc(100vw - 2em);
+  background-color: #222129;
+  padding: 0 1em 2em;
+`;
+
+const ComponentListContainer = styled.div`
+  display:flex;
+  flex-direction: row;
+  justify-content: space-around;
+`;
+
+const ComponentTitle = styled.div`
+  text-align: center;
+`;
+
 const GoogleWrapper = styled.div`
   padding: 2em;
+`;
+
+const GithubWrapper = styled.div`
+
+`;
+
+const GithubStars = styled.div`
+  width: 200px;
+  height: 200px;
+  background-color: white;
+  color: black;
+  text-align:center;
+  align-items: center;
+`;
+
+const NPMWrapper = styled.div`
+
+`;
+
+const NPMDownloads = styled.div`
+  width: 100%;
+  height: 200px;
+  background-color: white;
+  color: black;
+  text-align:center;
 `;
 
 export default function Support() {
@@ -35,12 +79,35 @@ export default function Support() {
         <Description>
           {description}
         </Description>
-        <GoogleWrapper id="widget">
-          <GoogleTrends
-            type="TIMESERIES"
-            url="https://ssl.gstatic.com/trends_nrtr/2431_RC03/embed_loader.js"
-          />
-        </GoogleWrapper>
+        <ComponentContainer>
+          <GoogleWrapper id="widget">
+            <ComponentTitle>
+              Google Trends
+            </ComponentTitle>
+            <GoogleTrends
+              type="TIMESERIES"
+              url="https://ssl.gstatic.com/trends_nrtr/2431_RC03/embed_loader.js"
+            />
+          </GoogleWrapper>
+          <ComponentListContainer>
+            <GithubWrapper>
+              <ComponentTitle>
+                Github-Stars
+              </ComponentTitle>
+              <GithubStars>
+                TBD
+              </GithubStars>
+            </GithubWrapper>
+            <NPMWrapper>
+              <ComponentTitle>
+                npm Downloads
+              </ComponentTitle>
+              <NPMDownloads>
+                TBD
+              </NPMDownloads>
+            </NPMWrapper>
+          </ComponentListContainer>
+        </ComponentContainer>
       </Wrapper>
     </Layout>
   );
